787c95f533ee78a781aa0b0ef1366284d7aaaed5
Lizard53 <glynn.jacob@gmail.com>
2025-08-20 18:02:23 +0100

    deleted:    banner-grab.py

--- banner-grab.py
+++
@@ -1,7 +0,0 @@
-# Violent Python - A Cookbook for Hackers... by TJ. O'Connor
-import socket
-socket.setdefaulttimeout(2)
-s = socket.socket()
-s.connect(("192.168.122.213",21))
-ans = s.recv(1024)
-print(ans)
